c451b67973ad9af5881efd92facea56e4702a9de
Adrian-w-sieci <ziiielu@gmail.com>
2025-05-20 19:57:45 +0200

    konf tel

--- src/app/ui/fonts.tsx
+++ src/app/ui/fonts.tsx
@@ -1,6 +1,5 @@
 import { Nunito } from 'next/font/google';
 import { Sacramento } from 'next/font/google';
-import { Nunito } from 'next/font/google';
 
 export const custom_font = Nunito({ subsets: ['latin'], weight: '400' });
 export const sacramento = Sacramento({ subsets: ['latin'], weight: '400' });
